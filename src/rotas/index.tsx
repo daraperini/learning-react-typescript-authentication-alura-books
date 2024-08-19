9434d4c55035da2f798569aa55983b77ded00adf
Dara Lahm Perini <daraperini@gmail.com>
2024-08-19 11:33:00 -0300

    Criando página área logada

--- src/rotas/index.tsx
+++ src/rotas/index.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "../paginas/Home"
 import PaginaBase from "../paginas/PaginaBase"
+import AreaLogada from "../paginas/AreaLogada"
 
 
 const Rotas = () => {
     return (<Routes>
       <Route path='/' element={<PaginaBase />}>
         <Route path='/' element={<Home />} />
+        <Route path='/minha-conta' element={<AreaLogada />}/>
       </Route>
     </Routes>)
 }
